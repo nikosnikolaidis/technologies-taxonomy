4d0eb9dc3c332d0dd89b745638040f92910d2be3
Nikos Nikolaidis <it14189@uom.edu.gr>
2024-10-22 11:38:02 +0300

    Test commit for jdbc

---
+++ technology-github-taxonomy/src/main/java/JDBCDemo.java
@@ -0,0 +1,38 @@
+package com.vinayak.jdbc;
+
+import java.sql.*;
+
+public class JDBCDemo {
+  
+    public static void main(String args[])
+        throws SQLException, ClassNotFoundException
+    {
+        String driverClassName
+            = "sun.jdbc.odbc.JdbcOdbcDriver";
+        String url = "jdbc:odbc:XE";
+        String username = "scott";
+        String password = "tiger";
+        String query
+            = "insert into students values(109, 'bhatt')";
+
+        // Load driver class
+        Class.forName(driverClassName);
+
+        // Obtain a connection
+        Connection con = DriverManager.getConnection(
+            url, username, password);
+
+        // Obtain a statement
+        Statement st = con.createStatement();
+
+        // Execute the query
+        int count = st.executeUpdate(query);
+        System.out.println(
+            "number of rows affected by this query= "
+            + count);
+
+        // Closing the connection as per the
+        // requirement with connection is completed
+        con.close();
+    }
+}
